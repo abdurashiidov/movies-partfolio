37c55145b65fa661b0bb653bf414feebfc85e09d
abdurashiidov <abdurashiidov18@gmail.com>
2021-12-13 21:27:04 +0300

    add changes

--- main.js
+++ main.js
@@ -1,74 +1,25 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 let father = document.querySelector(".father")
 
 function render(data, ota){
-data.map(elem =>{
-    let div = document.querySelector("div")
-    let h1 = document.querySelector("h1")
-    let img = document.querySelector("img")
-    let p = document.querySelector("p")
+    data.map(elem =>{
+        let div = document.querySelector("div")
+        let h1 = document.querySelector("h1")
+        let img = document.querySelector("img")
+        let p = document.querySelector("p")
 
-    h1.textContent = elem.Title
-    img.setAttribute("src", elem.Images)
-    p.textContent = elem.Plot
+        h1.textContent = elem.Title
+        img.setAttribute("src", elem.Images)
+        p.textContent = elem.Plot
 
-    div.classList.add("boxs")
+        div.classList.add("boxs")
 
-    div.appendChild(img)
-    div.appendChild(h1)
-    div.appendChild(p)
+        div.appendChild(img)
+        div.appendChild(h1)
+        div.appendChild(p)
 
-    ota.appendChild(div)
+        ota.appendChild(div)
 
-})
+    })
 }
 render(films, father)
 
