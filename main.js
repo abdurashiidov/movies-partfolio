9e7f8f119cf0a3cdc747e9439fe667079db2c811
abdurashiidov <abdurashiidov18@gmail.com>
2021-12-13 21:23:23 +0300

    add changes

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 let father = document.querySelector(".father")
 
-function render(data, ota)
+function render(data, ota){
 data.map(elem =>{
     let div = document.querySelector("div")
     let h1 = document.querySelector("h1")
@@ -11,9 +11,17 @@ data.map(elem =>{
     img.setAttribute("src", elem.Images)
     p.textContent = elem.Plot
 
-    
+    div.classList.add("boxs")
+
+    div.appendChild(img)
+    div.appendChild(h1)
+    div.appendChild(p)
+
+    ota.appendChild(div)
 
 })
+}
+render(films, father)
 
 
 
