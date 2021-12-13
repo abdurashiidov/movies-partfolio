fe64ce79deab8ce103d501a66fe081fd5e9e7c6c
abdurashiidov <abdurashiidov18@gmail.com>
2021-12-13 19:38:09 +0300

    add change js

--- main.js
+++ main.js
@@ -1,121 +1,55 @@
-const Snow = (canvas, count, options) => {
-    const ctx = canvas.getContext('2d');
-    const snowflakes = [];
-  
-    const add = item => snowflakes.push(item(canvas));
-  
-    const update = () => _.forEach(snowflakes, el => el.update());
-  
-    const resize = () => {
-      ctx.canvas.width = canvas.offsetWidth;
-      ctx.canvas.height = canvas.offsetHeight;
-  
-      _.forEach(snowflakes, el => el.resized());
-    };
-  
-    const draw = () => {
-      ctx.clearRect(0, 0, canvas.offsetWidth, canvas.offsetHeight);
-      _.forEach(snowflakes, el => el.draw());
-    };
-  
-    const events = () => {
-      window.addEventListener('resize', resize);
-    };
-  
-    const loop = () => {
-      draw();
-      update();
-      animFrame(loop);
-    };
-  
-    const init = () => {
-      _.times(count, () => add(canvas => SnowItem(canvas, null, options)));
-      events();
-      loop();
-    };
-  
-    init(count);
-    resize();
-  
-    return { add, resize };
-  };
-  
-  const defaultOptions = {
-    color: 'orange',
-    radius: [0.5, 3.0],
-    speed: [1, 3],
-    wind: [-0.5, 3.0] };
-  
-  
-  const SnowItem = (canvas, drawFn = null, opts) => {
-    const options = { ...defaultOptions, ...opts };
-    const { radius, speed, wind, color } = options;
-    const params = {
-      color,
-      x: _.random(0, canvas.offsetWidth),
-      y: _.random(-canvas.offsetHeight, 0),
-      radius: _.random(...radius),
-      speed: _.random(...speed),
-      wind: _.random(...wind),
-      isResized: false };
-  
-    const ctx = canvas.getContext('2d');
-  
-    const updateData = () => {
-      params.x = _.random(0, canvas.offsetWidth);
-      params.y = _.random(-canvas.offsetHeight, 0);
-    };
-  
-    const resized = () => params.isResized = true;
-  
-    const drawDefault = () => {
-      ctx.beginPath();
-      ctx.arc(params.x, params.y, params.radius, 0, 2 * Math.PI);
-      ctx.fillStyle = params.color;
-      ctx.fill();
-      ctx.closePath();
-    };
-  
-    const draw = drawFn ?
-    () => drawFn(ctx, params) :
-    drawDefault;
-  
-    const translate = () => {
-      params.y += params.speed;
-      params.x += params.wind;
-    };
-  
-    const onDown = () => {
-      if (params.y < canvas.offsetHeight) return;
-  
-      if (params.isResized) {
-        updateData();
-        params.isResized = false;
-      } else {
-        params.y = 0;
-        params.x = _.random(0, canvas.offsetWidth);
-      }
-    };
-  
-    const update = () => {
-      translate();
-      onDown();
-    };
-  
-    return {
-      update,
-      resized,
-      draw };
-  
-  };
-  
-  const el = document.querySelector('.container');
-  const wrapper = document.querySelector('body');
-  const canvas = document.getElementById('snow');
-  
-  const animFrame = window.requestAnimationFrame ||
-  window.mozRequestAnimationFrame ||
-  window.webkitRequestAnimationFrame ||
-  window.msRequestAnimationFrame;
-  
-  Snow(canvas, 150, { color: 'white' });
+var c = document.getElementById('canv'), 
+    $ = c.getContext("2d");
+var w = c.width = window.innerWidth, 
+    h = c.height = window.innerHeight;
+
+Snowy();
+function Snowy() {
+  var snow, arr = [];
+  var num = 600, tsc = 1, sp = 1;
+  var sc = 1.3, t = 0, mv = 20, min = 1;
+    for (var i = 0; i < num; ++i) {
+      snow = new Flake();
+      snow.y = Math.random() * (h + 50);
+      snow.x = Math.random() * w;
+      snow.t = Math.random() * (Math.PI * 2);
+      snow.sz = (100 / (10 + (Math.random() * 100))) * sc;
+      snow.sp = (Math.pow(snow.sz * .8, 2) * .15) * sp;
+      snow.sp = snow.sp < min ? min : snow.sp;
+      arr.push(snow);
+    }
+  go();
+  function go(){
+    window.requestAnimationFrame(go);
+      $.clearRect(0, 0, w, h);
+      $.fillStyle = 'hsla(242, 95%, 3%, 1)';
+      $.fillRect(0, 0, w, h);
+      $.fill();
+        for (var i = 0; i < arr.length; ++i) {
+          f = arr[i];
+          f.t += .05;
+          f.t = f.t >= Math.PI * 2 ? 0 : f.t;
+          f.y += f.sp;
+          f.x += Math.sin(f.t * tsc) * (f.sz * .3);
+          if (f.y > h + 50) f.y = -10 - Math.random() * mv;
+          if (f.x > w + mv) f.x = - mv;
+          if (f.x < - mv) f.x = w + mv;
+          f.draw();}
+ }
+ function Flake() {
+   this.draw = function() {
+      this.g = $.createRadialGradient(this.x, this.y, 0, this.x, this.y, this.sz);
+      this.g.addColorStop(0, 'hsla(255,255%,255%,1)');
+      this.g.addColorStop(1, 'hsla(255,255%,255%,0)');
+      $.moveTo(this.x, this.y);
+      $.fillStyle = this.g;
+      $.beginPath();
+      $.arc(this.x, this.y, this.sz, 0, Math.PI * 2, true);
+      $.fill();}
+  }
+}
+
+window.addEventListener('resize', function(){
+  c.width = w = window.innerWidth;
+  c.height = h = window.innerHeight;
+}, false);
